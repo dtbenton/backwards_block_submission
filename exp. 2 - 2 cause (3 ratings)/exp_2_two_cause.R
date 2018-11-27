e2a420ef9f4ca34aa25038d49f1f8aeaef111925
Deon T. Benton <dtbenton@andrew.cmu.edu>
2018-11-26 22:12:51 -0500

    t

--- exp. 2 - 2 cause (3 ratings)/exp_2_two_cause.R
+++ exp. 2 - 2 cause (3 ratings)/exp_2_two_cause.R
@@ -57,3 +57,38 @@ D_tall$sex = revalue(x = as.factor(D_tall$sex),
 D_tall = D_tall[,c(1,2,3,6,7,5,4)]
 D_tall$condition = NULL
 D_tall$row.names = NULL
+
+
+
+########################################################
+#############                              #############
+#############      Assumption Checks       #############
+#############                              #############
+########################################################
+# NORMALITY CHECK
+D_tall$norm_col = rep(1:24, times=24)
+hist(D_tall$measure[D_tall$norm_col==ii], breaks=5)
+shapiro.ps = rep(0,24)
+for(i in 1:16) {
+  shap.calc = shapiro.test(D_tall$measure[D_tall$norm_col==i])
+  shapiro.ps[i] = shap.calc$p.value
+}
+
+
+# EQUAL VARIANCE CHECK
+#box plots
+boxplot(D_tall$measure~D_tall$norm_col) 
+
+# formal test of equal variance
+leveneTest(D_tall$measure, as.factor(D_tall$norm_col), center=median) # used 'median' because it's a better measure of central tendency given the non-normality
+
+
+
+# ASSUMPTION CHECK SUMMARY
+# Based on the analyses above, there is a clear violation of the multi-variate normality and 
+# the homoskedasticity assumptions. 
+# Violations were indicated by a p-value of less than .005 for 22 of the 24 tests.
+# Conventional parametric tests, therefore, are not appropriate, and so subsequent
+# confidence intervals will be estimated using boostrapping and p-values will be
+# obtained using permutation testing. Planned comparisons were also conducted using
+# Wilcoxon paired sign-ranked tests.

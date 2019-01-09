a968cf3e5742df478417a10a47ca386a70a98974
Deon T. Benton <dtbenton@andrew.cmu.edu>
2019-01-09 17:33:05 -0500

    s

--- neural_network_scipts/fauxtest.ex
+++ neural_network_scipts/fauxtest.ex
@@ -1,17 +1,63 @@
-name:AonBonCoff7
+name:A
 I:	
 (Object_A)                        
  1 1 1 1 1 1 1 1 1 1 1 0
 (Position_A)    
- 1 0
+ 0 0
 (Object_B)                        
- 0 1 1 1 1 1 1 1 1 1 1 1
+ 0 0 0 0 0 0 0 0 0 0 0 0 
 (Position_B)    
- 1 0
+ 0 0
+(Object_C)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_C)    
+ 0 0
+(Input_Activation)    
+ 0
+
+T:	
+(Output_Activation)    
+ 1 
+;
+
+
+name:B
+I:	
+(Object_A)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_A)    
+ 0 0
+(Object_B)                        
+ 0 1 1 1 1 1 1 1 1 1 1 1 
+(Position_B)    
+ 0 0
+(Object_C)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_C)    
+ 0 0
+(Input_Activation)    
+ 0 
+
+T:	
+(Output_Activation)    
+ 1
+;
+
+
+name:C
+I:	
+(Object_A)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_A)    
+ 0 0
+(Object_B)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0 
+(Position_B)    
+ 0 0
 (Object_C)                        
  1 1 1 1 1 1 1 1 1 1 1 1
 (Position_C)    
- 0 1
+ 0 0
 (Input_Activation)    
  0 
 
@@ -19,26 +65,3 @@ T:
 (Output_Activation)    
  1 
 ;
-
-name:AonBonCoff8
-I:	
-(Object_A)                        
- 0 0 0 0 0 0 0 0 0 0 0 0
-(Position_A)    
- 0 0
-(Object_B)                        
- 0 0 0 0 0 0 0 0 0 0 0 0
-(Position_B)    
- 0 0
-(Object_C)                        
- 1 1 1 1 1 1 1 1 1 1 1 1
-(Position_C)    
- 0 1
-(Input_Activation)    
- 0 
-
-T:	
-(Output_Activation)    
- 0 
-;
-

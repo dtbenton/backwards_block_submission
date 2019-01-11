9e147eecf887c6eb22cd4513906f31aecfe38e37
Deon T. Benton <dtbenton@andrew.cmu.edu>
2019-01-11 17:00:03 -0500

    s

--- neural_network_scipts/AB_backwards_blocking.ex
+++ neural_network_scipts/AB_backwards_blocking.ex
@@ -42,3 +42,25 @@ T:
  0 
 ;
 
+
+name:AonBonCoff9
+I:	
+(Object_A)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_A)    
+ 0 0
+(Object_B)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_B)    
+ 0 0
+(Object_C)                        
+ 1 1 1 1 1 1 1 1 1 1 1 1
+(Position_C)    
+ 0 1
+(Input_Activation)    
+ 0 
+
+T:	
+(Output_Activation)    
+ 1 
+;
--- neural_network_scipts/A_backwards_blocking.ex
+++ neural_network_scipts/A_backwards_blocking.ex
@@ -13,9 +13,100 @@ I:
 (Position_C)    
  0 1
 (Input_Activation)    
- 0 1
+ 0 
 
 T:	
 (Output_Activation)    
- 1 0
+ 1 
+;
+
+name:AonBonCoff8
+I:	
+(Object_A)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_A)    
+ 0 0
+(Object_B)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_B)    
+ 0 0
+(Object_C)                        
+ 1 1 1 1 1 1 1 1 1 1 1 1
+(Position_C)    
+ 0 1
+(Input_Activation)    
+ 0 
+
+T:	
+(Output_Activation)    
+ 0 
+;
+
+
+name:AonBonCoff9
+I:	
+(Object_A)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_A)    
+ 0 0
+(Object_B)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_B)    
+ 0 0
+(Object_C)                        
+ 1 1 1 1 1 1 1 1 1 1 1 1
+(Position_C)    
+ 0 1
+(Input_Activation)    
+ 0 
+
+T:	
+(Output_Activation)    
+ 1 
+;
+
+
+name:AonBoffCoff10
+I:	
+(Object_A)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_A)    
+ 0 0
+(Object_B)                        
+ 0 1 1 1 1 1 1 1 1 1 1 1
+(Position_B)    
+ 0 1
+(Object_C)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_C)    
+ 0 0
+(Input_Activation)    
+ 0 
+
+T:	
+(Output_Activation)    
+ 0 
+;
+
+
+name:AonBoffCoff11
+I:	
+(Object_A)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_A)    
+ 0 0
+(Object_B)                        
+ 0 1 1 1 1 1 1 1 1 1 1 1
+(Position_B)    
+ 0 1
+(Object_C)                        
+ 0 0 0 0 0 0 0 0 0 0 0 0
+(Position_C)    
+ 0 0
+(Input_Activation)    
+ 0 
+
+T:	
+(Output_Activation)    
+ 1 
 ;
